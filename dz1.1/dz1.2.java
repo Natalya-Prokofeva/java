e081971ec230b1d7ae39d254ab23bbd17b68aa66
Natalya <159589443+Natalya-Prokofeva@users.noreply.github.com>
2024-09-09 20:27:53 +0300

    Update dz1.2.java

--- dz1.1/dz1.2.java
+++ dz1.1/dz1.2.java
@@ -1,1 +1,21 @@
+/**Задача 2. Вывод всех четных чисел от 1 до 100
+Напишите метод printEvenNums, который выведет на экран все четные числа
+в промежутке от 1 до 100, каждое на новой строке.*/
 
+public class EvenNumbersPrinter {
+
+    /**
+     * Выводит все четные числа от 1 до 100.
+     */
+    public static void printEvenNums() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printEvenNums();
+    }
+}
