6306ee3275e421dc4f513eb4402874b9feb46945
Natalya <159589443+Natalya-Prokofeva@users.noreply.github.com>
2024-09-09 20:30:11 +0300

    Update dz.1.3.java

--- dz1.1/dz.1.3.java
+++ dz1.1/dz.1.3.java
@@ -1,1 +1,30 @@
+/**Задача 3. Подсчет суммы цифр числа
+Напишите метод sumDigits, который принимает целое число n и возвращает
+сумму его цифр.*/
+
+public class DigitSumCalculator {
+    /**
+     * Вычисляет сумму цифр числа.
+     * 
+     * @param n целое число
+     * @return сумма цифр числа
+     */
+    public static int sumDigits(int n) {
+        int sum = 0;
+        n = Math.abs(n); // Обрабатываем случай отрицательного числа
+
+        while (n > 0) {
+            sum += n % 10; // Добавляем последнюю цифру к сумме
+            n /= 10; // Убираем последнюю цифру
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int number = 12345;
+        int result = sumDigits(number);
+        System.out.println("Сумма цифр числа " + number + " равна " + result);
+    }
+}
 
