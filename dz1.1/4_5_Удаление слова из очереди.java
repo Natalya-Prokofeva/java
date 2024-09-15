19ddd05261854e48dfee21c39de1edc1724caf1a
Natalya <159589443+Natalya-Prokofeva@users.noreply.github.com>
2024-09-15 20:15:39 +0300

    Update 4_5_Удаление слова из очереди.java

--- dz1.1/4_5_Удаление слова из очереди.java
+++ dz1.1/4_5_Удаление слова из очереди.java
@@ -2,3 +2,35 @@
 Реализуйте метод removeAllOccurrences в классе DequeTasks, который
 принимает Deque<String> и строку value. Метод должен удалить все
 вхождения строки value из очереди.*/
+
+import java.util.Deque;
+import java.util.LinkedList;
+class DequeTasks {
+public static void removeAllOccurrences(Deque<String>
+deque, String value) {
+// Напишите свое решение ниже
+}
+}
+// Не удаляйте этот класс - он нужен для вывода результатов на
+экран и проверки
+public class Printer {
+public static void main(String[] args) {
+Deque<String> deque = new LinkedList<>();
+String value;
+if (args.length < 2) {
+// При отправке кода на Выполнение, вы можете
+варьировать эти параметры
+deque.add("apple");
+deque.add("banana");
+deque.add("apple");
+deque.add("cherry");
+value = "apple";
+} else {
+for (int i = 0; i < args.length - 1; i++) {
+deque.add(args[i]);
+}
+value = args[args.length - 1];
+}
+DequeTasks.removeAllOccurrences(deque, value);
+}
+}
